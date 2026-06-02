380d432e80f0d43dfa349643f0b804609ce00ba1
Arxivus <deduykhin_evgenii@mail.ru>
2026-06-02 22:42:11 +0500

    Add Auth, works-page

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
+from .views import RegisterView
 from main import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('register/', RegisterView.as_view(), name='register'),
     path('works/', views.worksPage, name='works'),
     path('feedback/', views.feedbackPage, name='feedback'),
     path('blog/', views.blogPage, name='blog'),
+    
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import WorksImages, Post, Review
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.contrib.auth import login
 
 def home(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
     return render(request, 'index.html')
 
 
@@ -16,3 +22,16 @@ def feedbackPage(request):
 def blogPage(request):
     posts = Post.objects.all().order_by('-date_posted')
     return render(request, 'blog.html', {'posts': posts})
+
+
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'registration/register.html'
+    success_url = reverse_lazy('home')
+    
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        user = form.save()
+        login(self.request, user)
+        
+        return response
